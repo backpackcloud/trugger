079d0a95cd7fc141fcef39cf3a7a065684514ca0
Ataxexe <ataxexe@gmail.com>
2012-03-10 17:23:13 -0300

    Copyright update

--- src/main/java/net/sf/trugger/validation/validator/pt/br/CNPJValidator.java
+++ src/main/java/net/sf/trugger/validation/validator/pt/br/CNPJValidator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2011 Marcelo Varella Barca Guimarães
+ * Copyright 2009-2012 Marcelo Varella Barca Guimarães
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  *
