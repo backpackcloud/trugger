c8d6df85128bd57dc9aaebec98d03149d5d21b0f
Ataxexe <ataxexe@devnull.tools>
2017-06-27 11:14:26 -0300

    rename file

--- src/test/java/tools/devnull/trugger/OptionalObjectTest.java
+++ src/test/java/tools/devnull/trugger/OptionalObjectTest.java
@@ -37,7 +37,7 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-public class OptionalTest {
+public class OptionalObjectTest {
 
   private Object value = new Object();
 
